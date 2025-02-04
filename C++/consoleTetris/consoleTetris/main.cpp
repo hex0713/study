48b3b5818cc5e05d90820cda731c6ea77572d9ab
Bone <hex0713@gmail.com>
2025-02-04 22:38:16 +0900

    main.cpp 구조 작성
    main.cpp 구조 작성
     1. init(), update(), render() 구성으로 될 예정
     2. mainLoop() 내에서 동작할 예정
     3. mainLoop() 반환값이 true 이면 정상 종료 된 것으로 판단할 예정

--- C++/consoleTetris/consoleTetris/main.cpp
+++ C++/consoleTetris/consoleTetris/main.cpp
@@ -2,9 +2,45 @@
 
 using namespace std;
 
+void init();
+void update();
+void render();
+int mainLoop();
+
 int main()
 {
+	if (mainLoop())
+	{
+		// 성공적으로 종료 되었습니다.
+	}
 
+	
 
 	return 0;
+}
+
+int mainLoop()
+{
+	init();
+
+	while (true)
+	{
+		update();
+		render();
+	}
+
+	return true;
+}
+
+void init()
+{
+
+}
+
+void update()
+{
+}
+void render()
+{
+
 }
